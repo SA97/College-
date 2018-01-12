ebe40a2f1459600a06652bd81c4a6e8a42829fcd
SA97 <33417605+SA97@users.noreply.github.com>
2018-01-12 09:43:16 +0000

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Operations
+
+{
+	
+    public static void main(String args[])
+    {
+	public int div(int a, int b){
+			return a/b;
+	}
+
+	public int mul(int a, int b){
+		return a*b;
+	}
+
+	public int sub(int a, int b){
+		return a - b;
+	}
+
+	public int add(int a, int b){
+		return a + b;
+	}
+    }
+}	
